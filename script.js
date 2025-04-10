d77130b029f7b639bf68c88209811d5fe8f8b3df
Natiele <natiele887@gmail.com>
2025-04-09 22:12:20 -0300

    Pegando os elementos no JavaScript

---
+++ script.js
@@ -0,0 +1,8 @@
+function colorDia() {
+    //pegar valor de campo de dia
+    let days = document.getElementById('day').value;
+    //pegar valor de campo de cor
+    let color = document.getElementById('color').value;
+    //pegar os elementos da tabela calendário
+    let calendar = document.getElementById('calendar');
+}
