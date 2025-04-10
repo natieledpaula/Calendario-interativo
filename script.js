22fc4ba933fb4add36a9153e342c2def85a9a870
Natiele <natiele887@gmail.com>
2025-04-09 22:18:31 -0300

    Feito a parte de verificação de campo

--- script.js
+++ script.js
@@ -5,4 +5,15 @@ function colorDia() {
     let color = document.getElementById('color').value;
     //pegar os elementos da tabela calendário
     let calendar = document.getElementById('calendar');
+
+    //Verificar se o campo de dia foi prenchido
+    if(days != '') {
+        alert('Por favor informar o dia');
+    } else if((days > 0) && (days < 31)) {
+        //Se o numero de dias está no intervalo aplicara a cor na celula do calendario
+        let td = calendar.getElementsByTagName('td') [parseInt(days)+ 1];
+        td.style.backgroundColor = color;
+    } else {
+        alert("Por favor informar um dia do calendário");
+    }
 }
